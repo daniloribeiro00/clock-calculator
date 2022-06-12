c374583e744f8145d0bd6c9c686407e9a03a9ee3
Danilo Ribeiro <danilo.ribeiro00d@gmail.com>
2022-06-12 13:32:21 -0300

    fixed imports and changed icons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,22 +18,34 @@ onMounted(() => {
 
 <template>
   <header class="flex w-full justify-between p-6">
-    <h1
-      class="whitespace-pre-line text-xl leading-none text-white sm:whitespace-normal sm:text-2xl"
-    >
-      {{ 'Calculadora de\n Horas Trabalhadas' }}
-    </h1>
+    <span class="flex items-center justify-center gap-3">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        class="h-5 w-5 pb-px text-white"
+        fill="currentColor"
+      >
+        <path
+          d="M256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512zM232 256C232 264 236 271.5 242.7 275.1L338.7 339.1C349.7 347.3 364.6 344.3 371.1 333.3C379.3 322.3 376.3 307.4 365.3 300L280 243.2V120C280 106.7 269.3 96 255.1 96C242.7 96 231.1 106.7 231.1 120L232 256z"
+        />
+      </svg>
+      <h1
+        class="whitespace-pre-line text-xl leading-none text-white sm:whitespace-normal sm:text-2xl"
+      >
+        {{ 'Calculadora de\n Horas Trabalhadas' }}
+      </h1>
+    </span>
     <div class="flex items-center justify-center gap-4">
       <Transition name="fade" mode="out-in">
         <p
           v-if="globalStore.mode === 'totalTime'"
-          class="whitespace-pre-line text-sm leading-tight text-white sm:whitespace-normal text-right"
+          class="whitespace-pre-line text-right text-sm leading-tight text-white sm:whitespace-normal"
         >
           {{ 'Total de\n Horas' }}
         </p>
         <p
           v-else
-          class="whitespace-pre-line text-sm leading-tight text-white sm:whitespace-normal text-right"
+          class="whitespace-pre-line text-right text-sm leading-tight text-white sm:whitespace-normal"
         >
           {{ 'Horário\n de Saída' }}
         </p>
